1bad419561725e8328bbd6fb924abd9014a792a8
Pavol Rusnak <pavol@rusnak.io>
2020-12-02 10:41:44 +0100

    fixup! fixup! feat(python): add btc get-descriptor command

--- python/src/trezorlib/cli/btc.py
+++ python/src/trezorlib/cli/btc.py
@@ -190,38 +190,26 @@ def get_public_node(client, coin, address, curve, script_type, show_display):
     }
 
 
-@cli.command()
-@click.option("-c", "--coin")
-@click.option(
-    "-n", "--account", required=True, type=int, help="account index (0 = first account)"
-)
-@click.option("-t", "--script-type", type=ChoiceType(INPUT_SCRIPTS), default="address")
-@click.option("-d", "--show-display", is_flag=True)
-@with_client
-def get_descriptor(client, coin, account, script_type, show_display):
-    """Get descriptor of given account."""
+def _get_descriptor(client, coin, account, script_type, show_display):
     coin = coin or DEFAULT_COIN
     if script_type == messages.InputScriptType.SPENDADDRESS:
         acc_type = 44
         fmt = "pkh({})"
-        account_type = "Legacy"
     elif script_type == messages.InputScriptType.SPENDP2SHWITNESS:
         acc_type = 49
         fmt = "sh(wpkh({}))"
-        account_type = "Compatibility Segwit"
     elif script_type == messages.InputScriptType.SPENDWITNESS:
         acc_type = 84
-        account_type = "Native Segwit"
         fmt = "wpkh({})"
     else:
-        raise click.ClickException("Unsupported account type")
+        raise ValueError("Unsupported account type")
 
     if coin is None or coin == "Bitcoin":
         coin_type = 0
     elif coin == "Testnet":
         coin_type = 1
     else:
-        raise click.ClickException("Unsupported coin")
+        raise ValueError("Unsupported coin")
 
     path = f"m/{acc_type}'/{coin_type}'/{account}'"
     n = tools.parse_path(path)
@@ -237,8 +225,26 @@ def get_descriptor(client, coin, account, script_type, show_display):
     fingerprint = pub.root_fingerprint if pub.root_fingerprint is not None else 0
     external = f"[{fingerprint:08x}{path[1:]}]{pub.xpub}/0/*"
     internal = f"[{fingerprint:08x}{path[1:]}]{pub.xpub}/1/*"
-
     return fmt.format(external), fmt.format(internal)
+
+
+@cli.command()
+@click.option("-c", "--coin")
+@click.option(
+    "-n", "--account", required=True, type=int, help="account index (0 = first account)"
+)
+@click.option("-t", "--script-type", type=ChoiceType(INPUT_SCRIPTS), default="address")
+@click.option("-d", "--show-display", is_flag=True)
+@with_client
+def get_descriptor(client, coin, account, script_type, show_display):
+    """Get descriptor of given account."""
+    try:
+        ds = _get_descriptor(client, coin, account, script_type, show_display)
+        click.echo()
+        for d in ds:
+            click.echo(d)
+    except ValueError as e:
+        raise click.ClickException(e.msg)
 
 
 #
